9b76e34c3576b7372bd3133a6b5ea079ed48c75e
Tharun Raj <tharun.2.linux@gmail.com>
2024-07-06 10:24:26 +0530

    add an active effect

--- app/main.js
+++ app/main.js
@@ -10,17 +10,18 @@ date.textContent = d;
 const navigationToggleButton = document.querySelector(".navbar__toggle-btn");
 const toggleIcon = document.querySelector(".navbar__toggle-btn--icon");
 let hidden = true;
+const navigationLinks = document.querySelector(".navbar__links");
 
 navigationToggleButton.addEventListener("click", () => {
   if (hidden) {
     navigationToggleButton.setAttribute("aria-expanded", true);
     toggleIcon.setAttribute("src", closeIcon);
     hidden = false;
-    document.querySelector(".navbar__links").style.display = "flex";
+    navigationLinks.classList.add("links-show");
   } else {
     navigationToggleButton.setAttribute("aria-expanded", false);
     toggleIcon.setAttribute("src", hamburgerIcon);
     hidden = true;
-    document.querySelector(".navbar__links").style.display = "none";
+    navigationLinks.classList.remove("links-show");
   }
 });
